d5f336e88f4a6fc342a433a34366c8602b2214c1
Kelly Cristina Neves Santos <98kellycris@gmail.com>
2022-06-29 13:11:17 -0300

    front-end

--- Back/src/app.js
+++ Back/src/app.js
@@ -2,6 +2,7 @@ import express from "express"
 import database from "./config/database.js"
 import userRouter from "./routes/userRouter.js"
 import dotenv from "dotenv-safe"
+import cors from "cors"
 
 dotenv.config()
 
@@ -9,6 +10,8 @@ dotenv.config()
 //database.on("erro", () => console.log("Ops! Houve algum erro!"))
 
 const app = express();
+
+app.use(cors())
 app.use(express.json());
 app.use("/user", userRouter);
 
